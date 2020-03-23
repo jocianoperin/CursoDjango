364ccd663d849919dc790876ee9849132c952703
Jociano Perin <jociano.perin@outlook.com>
2020-03-22 22:17:58 -0300

    aula34

--- aula34.py
+++ aula34.py
@@ -1,7 +1,12 @@
 secreto = 'perfume'
 digitadas = []
+chances = 3
 
 while True:
+    if chances == 0:
+        print('Você perde!')
+        break
+
     letra = input('Digite uma letra: ')
 
     if len(letra) > 1:
@@ -17,3 +22,22 @@ while True:
 
     else:
         print(f'A letra "{letra}" não existe na palvra secreta.')
+
+    secreto_temp = ''
+
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temp += letra_secreta
+        else:
+            secreto_temp += '*'
+
+    print(secreto_temp)
+
+    if secreto_temp == secreto:
+        print('Fim de jogo')
+        break
+    else:
+        print(f'A palavra secreta está assim : {secreto_temp}')
+
+    if letra not in secreto:
+        chances -= 1
