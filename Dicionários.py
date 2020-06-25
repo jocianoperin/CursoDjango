02884e858b393130a9c797cea9169cff54548d0a
Jociano Perin <jociano.perin@outlook.com>
2020-06-25 13:31:26 -0300

    ajuste

--- Dicionários.py
+++ Dicionários.py
@@ -1,1 +1,10 @@
 d1 = dict(chave1='Valor da chave1', chave2='Valor da chave2')
+
+if 'chave2' in d1:
+    print (d1['chave2'])
+else:
+    print('Não existe')
+
+d1.update({'chave2':'novo valor'})
+
+print(d1)
